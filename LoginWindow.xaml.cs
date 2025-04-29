997b44c9969ddc445d4394dfd9d2de2a37e50731
Norsty YT <adamrothbauer@gmail.com>
2025-04-29 21:47:08 +0200

    Lehce upraven vzhled LoginWindow, taktéž zpřehledněn jeho kód, po úspěšném přihlášení přesměrovává na další okno.

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -1,19 +1,39 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace Zaverecny_Projekt
 {
     public partial class LoginWindow : Window
     {
+        /*                  TO DO LIST
+         * 
+         *  Upravit hover effect u tlačítka "Login_Button"
+         *  Přidat label varování zvlášt pro login a heslo
+         * 
+         * 
+         */
+
         public LoginWindow()
         {
             InitializeComponent();
         }
 
+        //---------------------------------------------------------------------------\\
+        //                          Input Field Text Effects                         \\
+        //---------------------------------------------------------------------------\\
+
+        readonly string loginText = "Uživatelské jméno";
+        readonly string passwordText = "Heslo";
+        readonly int fontSize_focused = 20;
+        readonly int fontSize_unfocused = 16;
+        readonly SolidColorBrush color_unfocused = Brushes.Gray;
+        readonly SolidColorBrush color_focused = Brushes.Black;
+
         private void Login_TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (Login_TextBox.Text == "Uživatelské jméno")
+            if (Login_TextBox.Text == loginText)
             {
-                Login_TextBox.Text = string.Empty;
+                ModifyLoginTextVisuals(true);
             }
         }
 
@@ -21,15 +41,15 @@ namespace Zaverecny_Projekt
         {
             if (Login_TextBox.Text == string.Empty)
             {
-                Login_TextBox.Text = "Uživatelské jméno";
+                ModifyLoginTextVisuals(false);
             }
         }
 
         private void Password_TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (Password_TextBox.Text == "Heslo")
+            if (Password_TextBox.Text == passwordText)
             {
-                Password_TextBox.Text = string.Empty;
+                ModifyPasswordTextVisuals(true);
             }
         }
 
@@ -37,22 +57,70 @@ namespace Zaverecny_Projekt
         {
             if (Password_TextBox.Text == string.Empty)
             {
-                Password_TextBox.Text = "Heslo";
+                ModifyPasswordTextVisuals(false);
             }
         }
 
+        private void ModifyLoginTextVisuals(bool focused)
+        {
+            if (focused)
+            {
+                Login_TextBox.Text = string.Empty;
+
+                Login_TextBox.FontSize = fontSize_focused;
+                Login_TextBox.Foreground = color_focused;
+                Login_TextBox.FontStyle = FontStyles.Normal;
+            }
+            else
+            {
+                Login_TextBox.Text = loginText;
+
+                Login_TextBox.FontSize = fontSize_unfocused;
+                Login_TextBox.Foreground = color_unfocused;
+                Login_TextBox.FontStyle = FontStyles.Italic;
+            }
+        }
+
+        private void ModifyPasswordTextVisuals(bool focused)
+        {
+            if (focused)
+            {
+                Password_TextBox.Text = string.Empty;
+
+                Password_TextBox.FontSize = fontSize_focused;
+                Password_TextBox.Foreground = color_focused;
+                Password_TextBox.FontStyle = FontStyles.Normal;
+            }
+            else
+            {
+                Password_TextBox.Text = passwordText;
+
+                Password_TextBox.FontSize = fontSize_unfocused;
+                Password_TextBox.Foreground = color_unfocused;
+                Password_TextBox.FontStyle = FontStyles.Italic;
+            }
+        }
+
+        //---------------------------------------------------------------------------\\
+        //                          User Authentication                              \\
+        //---------------------------------------------------------------------------\\
+
         private void Login_Button_Click(object sender, RoutedEventArgs e)
         {
-            if (Login_TextBox.Text.ToLower() != "rothbad" || Password_TextBox.Text != "heslo123")
+            if (Login_TextBox.Text.ToLower() != "student" || Password_TextBox.Text != "zlab123")
             {
                 Warning_Label.Visibility = Visibility.Visible;
 
-                Login_TextBox.Text = "Uživatelské jméno";
-                Password_TextBox.Text = "Heslo";
+                ModifyLoginTextVisuals(false);
+                ModifyPasswordTextVisuals(false);
             }
             else
             {
                 Warning_Label.Visibility = Visibility.Hidden;
+
+                Zak_vzhled studentWindow = new();
+                this.Close();
+                studentWindow.ShowDialog();
             }
         }
     }
