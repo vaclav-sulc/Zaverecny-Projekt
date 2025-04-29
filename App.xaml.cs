2c99d507b272575728842cb00f9ab9265d967f1a
sulcvac <vasa.sulc@gmail.com>
2025-04-29 12:44:30 +0200

    Přidejte soubory projektu.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Zaverecny_Projekt
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
