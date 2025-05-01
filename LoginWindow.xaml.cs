a2f338c699644e0cd812aa5b8af2fc5ce93e2c4f
Václav Šulc <sulcvac@student.uzlabina.cz>
2025-05-01 16:01:58 +0200

    implementoval jsem WPF UI a upravil login page

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System.Windows;
 using System.Windows.Media;
+using Wpf.Ui.Controls;
 using MySql.Data.MySqlClient;
 
 namespace Zaverecny_Projekt
 {
-    public partial class LoginWindow : Window
+    public partial class LoginWindow : FluentWindow
     {
         /*                  TO DO LIST
          * 
@@ -20,100 +21,17 @@ namespace Zaverecny_Projekt
         }
 
         //---------------------------------------------------------------------------\\
-        //                          Input Field Text Effects                         \\
-        //---------------------------------------------------------------------------\\
-
-        readonly string loginText = "Uživatelské jméno";
-        readonly string passwordText = "Heslo";
-        readonly int fontSize_focused = 20;
-        readonly int fontSize_unfocused = 16;
-        readonly SolidColorBrush color_unfocused = Brushes.Gray;
-        readonly SolidColorBrush color_focused = Brushes.Black;
-
-        private void Login_TextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (Login_TextBox.Text == loginText)
-            {
-                ModifyLoginTextVisuals(true);
-            }
-        }
-
-        private void Login_TextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (Login_TextBox.Text == string.Empty)
-            {
-                ModifyLoginTextVisuals(false);
-            }
-        }
-
-        private void Password_TextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (Password_TextBox.Text == passwordText)
-            {
-                ModifyPasswordTextVisuals(true);
-            }
-        }
-
-        private void Password_TextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (Password_TextBox.Text == string.Empty)
-            {
-                ModifyPasswordTextVisuals(false);
-            }
-        }
-
-        private void ModifyLoginTextVisuals(bool focused)
-        {
-            if (focused)
-            {
-                Login_TextBox.Text = string.Empty;
-
-                Login_TextBox.FontSize = fontSize_focused;
-                Login_TextBox.Foreground = color_focused;
-                Login_TextBox.FontStyle = FontStyles.Normal;
-            }
-            else
-            {
-                Login_TextBox.Text = loginText;
-
-                Login_TextBox.FontSize = fontSize_unfocused;
-                Login_TextBox.Foreground = color_unfocused;
-                Login_TextBox.FontStyle = FontStyles.Italic;
-            }
-        }
-
-        private void ModifyPasswordTextVisuals(bool focused)
-        {
-            if (focused)
-            {
-                Password_TextBox.Text = string.Empty;
-
-                Password_TextBox.FontSize = fontSize_focused;
-                Password_TextBox.Foreground = color_focused;
-                Password_TextBox.FontStyle = FontStyles.Normal;
-            }
-            else
-            {
-                Password_TextBox.Text = passwordText;
-
-                Password_TextBox.FontSize = fontSize_unfocused;
-                Password_TextBox.Foreground = color_unfocused;
-                Password_TextBox.FontStyle = FontStyles.Italic;
-            }
-        }
-
-        //---------------------------------------------------------------------------\\
         //                          User Authentication                              \\
         //---------------------------------------------------------------------------\\
 
-        private void Login_Button_Click(object sender, RoutedEventArgs e)
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             using MySqlConnection mySqlConnection = new("server=sql7.freesqldatabase.com;user=sql7776236;password=rakYbIVDef;database=sql7776236;");
             try
             {
                 mySqlConnection.Open();
 
-                string sqlQuery = $"SELECT * FROM zlabgrade WHERE login = \"{Login_TextBox.Text.ToLower()}\" AND heslo = \"{Password_TextBox.Text}\"";
+                string sqlQuery = $"SELECT * FROM zlabgrade WHERE login = \"{LoginTextBox.Text.ToLower()}\" AND heslo = \"{PasswordTextBox.Password}\"";
                 MySqlCommand command = new(sqlQuery, mySqlConnection);
                 
                 using MySqlDataReader dataReader = command.ExecuteReader();
@@ -121,7 +39,7 @@ namespace Zaverecny_Projekt
                 {
                     dataReader.Read();
 
-                    Warning_Label.Visibility = Visibility.Hidden;
+                    WarningBar.Visibility = Visibility.Hidden;
 
                     switch (dataReader["role"])
                     {
@@ -145,10 +63,7 @@ namespace Zaverecny_Projekt
                 }
                 else
                 {
-                    Warning_Label.Visibility = Visibility.Visible;
-
-                    ModifyLoginTextVisuals(false);
-                    ModifyPasswordTextVisuals(false);
+                    WarningBar.IsOpen = true;
                 }
             }
             catch (Exception exception)
