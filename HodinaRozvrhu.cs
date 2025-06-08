274cce00abd798397517bb3801a74341d6e03d35
Václav Šulc <sulcvac@student.uzlabina.cz>
2025-06-08 14:54:59 +0200

    pridani rozvrhu do StudentWindow

---
+++ HodinaRozvrhu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZlabGrade
+{
+    public class HodinaRozvrhu
+    {
+        public string Den { get; set; }
+        public int Hodina { get; set; }
+        public string Predmet { get; set; }
+        public string Ucitel { get; set; }
+        public string Mistnost { get; set; }
+
+        public HodinaRozvrhu(string den, int hodina, string predmet, string ucitel, string mistnost)
+        {
+            Den = den;
+            Hodina = hodina;
+            Predmet = predmet;
+            Ucitel = ucitel;
+            Mistnost = mistnost;
+        }
+
+    }
+}
